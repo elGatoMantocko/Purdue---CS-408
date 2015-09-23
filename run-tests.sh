f47453b410474005949d7137310b844e25cea8a5
Calvin Sauer <calvin.j.sauer@gmail.com>
2015-09-23 15:22:10 -0400

    Removed debugging code

--- run-tests.sh
+++ run-tests.sh
@@ -3,4 +3,3 @@ cd tests/cucumber
 npm install
 cd ../..
 VELOCITY_CI=1 SELENIUM_BROWSER=phantomjs meteor --test
-cat /home/travis/build/elGatoMantocko/BetterBackground/.meteor/local/log/cucumber.log
