330d4c0d43778b4a374919133fa627a2a2ce5b00
Calvin Sauer <calvin.j.sauer@gmail.com>
2015-09-23 15:14:53 -0400

    Explicitly use phantomjs

--- run-tests.sh
+++ run-tests.sh
@@ -2,5 +2,5 @@
 cd tests/cucumber
 npm install
 cd ../..
-VELOCITY_CI=1 meteor --test
+VELOCITY_CI=1 SELENIUM_BROWSER=phantomjs meteor --test
 cat /home/travis/build/elGatoMantocko/BetterBackground/.meteor/local/log/cucumber.log
