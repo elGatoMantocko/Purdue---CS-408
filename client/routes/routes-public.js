0b95828c057ee469dc173eb4e1dd63590d703cf3
Elliott Mantock <emantock@gmail.com>
2015-09-24 08:27:59 -0700

    set up the home template to be ready for content

--- client/routes/routes-public.js
+++ client/routes/routes-public.js
@@ -1,6 +1,6 @@
 FlowRouter.route('/', {
   action: function() {
-    BlazeLayout.render("layoutDefault", {header: "navigation", main: "construction"});
+    BlazeLayout.render("layoutDefault", {header: "navigation", main: "home"});
   }
 });
 
