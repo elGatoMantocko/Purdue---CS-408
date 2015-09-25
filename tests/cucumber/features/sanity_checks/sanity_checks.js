853816824109583668b9b41e45c8edbd19ada1eb
Calvin Sauer <calvin.j.sauer@gmail.com>
2015-09-25 17:13:18 -0400

    Removed redundant line of code

--- tests/cucumber/features/sanity_checks/sanity_checks.js
+++ tests/cucumber/features/sanity_checks/sanity_checks.js
@@ -4,10 +4,7 @@ module.exports = function () {
 
   this.Then(/^I should see the title "([^"]*)"$/, function (expectedTitle) {
     client.waitForExist('title');
-
     var actual = client.getTitle();
-    var expected = expectedTitle;
-
-    expect(actual).toBe(expected);
+    expect(actual).toBe(expectedTitle);
   });
 }
