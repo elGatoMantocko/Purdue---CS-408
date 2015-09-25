0f14551b361d52b51cab1427f26b29246c0239b3
Elliott Mantock <emantock@gmail.com>
2015-09-25 09:58:37 -0700

    created auth cucumber test directory

---
+++ tests/cucumber/features/authentication/authentication.feature
@@ -0,0 +1,14 @@
+Feature: Login
+  As a registered user
+  I want to login to my account
+  So that I can manage my account
+
+  #Background:
+    #Given I am signed out
+
+  @dev
+  Scenario: A user can login with valid information
+    Given I am on the home page
+    When I click the login button
+    And I enter my authentication information
+    Then I should be logged in
---
+++ tests/cucumber/features/authentication/authentication.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// You can include npm dependencies for support files in tests/cucumber/package.json
+var _ = require('underscore');
+// You can use normal require here, cucumber is NOT run in a Meteor context (by design)
+var url = require('url');
+
+module.exports = function () {
+
+  this.When(/^I click on the login button$/, function() {
+    client.url(process.env.ROOT_URL);
+
+    client.waitForExist(".container-fluid", 1000);
+    client.waitForVisible(".container-fluid", 1000);
+
+    client.click("#login-btn");
+  });
+
+  this.When(/^I enter my authentication information$/, function() {
+    return 1;
+  });
+
+  this.Then(/^I should be logged in$/, function() {
+    return 1;
+  });
+
+};
