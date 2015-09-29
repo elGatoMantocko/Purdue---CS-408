9aaa3218f741d95587613b2bf5b2ff57a588216a
Elliott Mantock <emantock@gmail.com>
2015-09-29 13:03:46 -0400

    changing how travis runs the tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+npm install -g velocity-cli
 cd tests/cucumber
 npm install
 cd ../..
-VELOCITY_CI=1 JASMINE_BROWSER=PhantomJS SELENIUM_BROWSER=phantomjs meteor --test
+VELOCITY_CI=1 CUCUMBER=0 JASMINE_BROWSER=PhantomJS SELENIUM_BROWSER=phantomjs velocity test-app --ci 
+VELOCITY_CI=1 JASMINE_SERVER_INTEGRATION=0 JASMINE_BROWSER=PhantomJS SELENIUM_BROWSER=phantomjs velocity test-app --ci
