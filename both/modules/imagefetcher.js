afe475bdf29d151eef59c607d0b04162406d2057
Calvin Sauer <calvin.j.sauer@gmail.com>
2015-10-01 18:12:32 -0400

    Updated image fetcher to ES6 syntax

--- both/modules/imagefetcher.js
+++ both/modules/imagefetcher.js
@@ -12,18 +12,17 @@ let urlsOnly = (channel, page) => {
 
   // We need to make a Future object because the search async call needs to be synchronous
   var fut = new Future();
-  var options = new Object();
-  options.callback = function(err, images) {
-    if (err) return console.log(err);
-    var urls = images.map(function(image) { return image.url; });
- 
-    fut.return(urls);
+  var options = {
+    callback(err, images) {
+      if (err) return console.log(err);
+      var urls = images.map(function(image) { return image.url; });
+   
+      fut.return(urls);
+    },
+    page: page
   };
-  if (page)
-    options.page = page;
   GoogleImages.search(channel.get('query'), options);
   return fut.wait();
-
 };
 
 Modules.both.getUrls = urlsOnly;
