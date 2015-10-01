ecc749fbe64dd9e7e4bc59071df3596563592bb0
Calvin Sauer <calvin.j.sauer@gmail.com>
2015-10-01 18:40:04 -0400

    Improve run tests script

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-sudo npm install -g velocity-cli
+npm install -g velocity-cli
 cd tests/cucumber
 npm install
 cd ../..
 CUCUMBER=0 JASMINE_BROWSER=PhantomJS velocity test-app --ci 
-if [ $? -ne 0 ]
+jasmine=$?
+JASMINE_SERVER_INTEGRATION=0 SELENIUM_BROWSER=phantomjs velocity test-app --ci
+cuke=$?
+if [ $jasmine -ne 0 -o $cuke -ne 0 ]
 then
   exit -1
 fi
-JASMINE_SERVER_INTEGRATION=0 SELENIUM_BROWSER=phantomjs velocity test-app --ci
