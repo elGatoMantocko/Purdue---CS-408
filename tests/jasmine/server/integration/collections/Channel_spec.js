9ce04ceaf33b707db63e31d711358fd563745038
Calvin Sauer <calvin.j.sauer@gmail.com>
2015-10-01 18:14:38 -0400

    Move ImageFetcher tests into a new file

--- tests/jasmine/server/integration/collections/Channel_spec.js
+++ tests/jasmine/server/integration/collections/Channel_spec.js
@@ -139,24 +139,4 @@ describe('Channel collection', function() {
       expect(Channel.find.calls.argsFor(0)[1].sort).toEqual({createdAt: -1});
     });
   });
-
-  describe('image fetcher', function() { 
-    it('should return 64 urls from the channel query', function() {
-      let channel = new Channel({
-        title: 'My Test Channel',
-        query: 'little flower ponies'
-      });
-      channel = Meteor.call('/channels/new', channel);
-
-      urls = Meteor.call('/channels/getUrls', channel);
-      expect(urls.length).toEqual(64);
-      for (i = 0; i < urls.length; i++) {
-        expect(urls[i]).not.toEqual('');
-      }
-
-      // Clean up 
-      channel.remove();
-    });
-  });
-
 });
