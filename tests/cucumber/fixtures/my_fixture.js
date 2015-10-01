028b017a3f32576e7650e76eaacd4a1ac651f299
Elliott Mantock <emantock@gmail.com>
2015-10-01 09:57:14 -0400

    added reset_users back and made code readable

--- tests/cucumber/fixtures/my_fixture.js
+++ tests/cucumber/fixtures/my_fixture.js
@@ -1,18 +1,30 @@
 'use strict';
 
 Meteor.methods({
+
+  'reset_users': function() {
+    // Remove all users
+    Meteor.users.remove({});
+  },
+
   'reset_user': function(useremail) {
     check(useremail, String);
 
-    var user = _.find(Meteor.users.find().fetch(), function(user) { 
+    // Array of Meteor user accounts
+    var users = Meteor.users.find().fetch();
+
+    // Find user based on email under nested emails array
+    var user = _.find(users, function(user) { 
       return _.find(user.emails, function(email) { 
         return email.address === useremail;
       });
     });
 
+    // If we found a user, remove it
     if (typeof user !== 'undefined') {
       console.log(user._id)
       Meteor.users.remove({_id: user._id});
     }
   }
+
 });
