370212a37f8dd5b84d9ad5bfc476fc2dc46c18c6
Calvin Sauer <calvin.j.sauer@gmail.com>
2015-10-01 16:45:49 -0400

    Attempt to use firefox browser

--- run_tests.sh
+++ run_tests.sh
@@ -4,5 +4,5 @@ cd tests/cucumber
 npm install
 cd ../..
 CUCUMBER=0 JASMINE_BROWSER=PhantomJS velocity test-app --ci 
-JASMINE_SERVER_INTEGRATION=0 velocity test-app --ci
+JASMINE_SERVER_INTEGRATION=0 CUCMBER_BROWSER=firefox velocity test-app --ci
 cat /home/travis/build/elGatoMantocko/BetterBackground/.meteor/local/log/cucumber.log
