84b8e1b4b81d289e142353323c8be67f1bd8a382
Calvin Sauer <calvin.j.sauer@gmail.com>
2015-10-01 18:34:09 -0400

    Rewrote bash script to fail after jasmine

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-npm install -g velocity-cli
+sudo npm install -g velocity-cli
 cd tests/cucumber
 npm install
 cd ../..
 CUCUMBER=0 JASMINE_BROWSER=PhantomJS velocity test-app --ci 
+if [ $? -ne 0 ]
+then
+  exit -1
+fi
 JASMINE_SERVER_INTEGRATION=0 SELENIUM_BROWSER=phantomjs velocity test-app --ci
