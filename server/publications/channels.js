18e6e20f3e50ee07c92ee06eb2e82b0f3e24765f
Calvin Sauer <calvin.j.sauer@gmail.com>
2015-10-02 11:55:43 -0400

    Add a myChannels publication

--- server/publications/channels.js
+++ server/publications/channels.js
@@ -4,3 +4,15 @@
 Meteor.publish('latestChannels', function(limit) {
   return Channel.getLatest(limit);
 });
+
+/*
+ * Publish my subscribed channels
+ */
+Meteor.publish('myChannels', function() {
+
+  if(!this.userId) {
+    this.error(new Meteor.Error('unauthorized', 'You must be logged in to get your subscriptions.'));
+  }
+  let user = User.findOne({_id: this.userId});
+  return user.getSubscriptions();
+});
--- tests/jasmine/client/integration/Channel_publications_spec.js
+++ tests/jasmine/client/integration/Channel_publications_spec.js
@@ -2,4 +2,8 @@ describe('Channel publications', function() {
   it('publishes the latest channels', function() {
     Meteor.subscribe('latestChannels');
   });
+
+  it('publishes my subscribed channels', function() {
+    Meteor.subscribe('myChannels');
+  });
 });
