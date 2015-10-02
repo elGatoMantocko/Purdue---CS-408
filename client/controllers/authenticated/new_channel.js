7292dcbd4c68108d3343a6ca2843415e76a7c5e9
Elliott Mantock <emantock@gmail.com>
2015-10-02 19:00:16 -0400

    cant use Astro validation

--- client/controllers/authenticated/new_channel.js
+++ client/controllers/authenticated/new_channel.js
@@ -1,11 +1,10 @@
-Template.newchannel.onCreated(function() {
-  this.channel = new Channel();
-});
-
 Template.newchannel.events({
 
   'change input': function(e, tmpl) {
     e.preventDefault();
+
+    if (!this.channel) 
+      this.channel = new Channel();
 
     var field = e.currentTarget;
     this.channel.set(field.id, field.value);
@@ -15,8 +14,7 @@ Template.newchannel.events({
   'submit form': function(e, tmpl) {
     e.preventDefault();
 
-    console.log(this.channel);
-
+    $("#add-channel").validate();
     Meteor.call('/channels/new', this.channel, function(err, res) {
       if (err) this.channel.catchValidationException();
       FlowRouter.go('/channels/' + res._id);
