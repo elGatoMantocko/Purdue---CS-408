91e9adf5b59a483658706afbc13f13fac07037a3
Elliott Mantock <emantock@gmail.com>
2015-10-02 15:44:29 -0400

    validate new channel

--- client/controllers/authenticated/new_channel.js
+++ client/controllers/authenticated/new_channel.js
@@ -1,16 +1,20 @@
 Template.newchannel.events({
+
   'change input': function(e, tmpl) {
     e.preventDefault();
 
     var field = e.currentTarget;
     this[field.id] = field.value;
-    this.validate(field.id);
   },
-  'click [name=submit]': function(e, tmpl) {
+
+  'submit form': function(e, tmpl) {
     e.preventDefault();
 
-    if (this.validate()) {
-      this.save();
+    var channel = new Channel({title: this.title, query: this.query});
+
+    if (channel.validate()) {
+      channel.save();
     }
   }
+
 });
