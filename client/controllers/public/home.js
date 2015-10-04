2900395eaf65cb37d01a6c969d4ed4599fa8ad92
Elliott Mantock <emantock@gmail.com>
2015-10-04 15:10:55 -0400

    added event listener for click channel link

--- client/controllers/public/home.js
+++ client/controllers/public/home.js
@@ -10,3 +10,9 @@ Template.home.helpers({
     return Channel.getLatest().fetch();
   }
 });
+
+Template.home.events({
+  'click .list-group-item': function(e, tmpl) {
+    e.preventDefault();
+  }
+});
