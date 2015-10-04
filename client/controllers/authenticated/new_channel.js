13ebfa54c2859381fc1168ad20a32993870559e0
Elliott Mantock <emantock@gmail.com>
2015-10-03 22:47:57 -0400

    trying to clean up channel validation

--- client/controllers/authenticated/new_channel.js
+++ client/controllers/authenticated/new_channel.js
@@ -1,24 +1,25 @@
+Template.newchannel.onCreated(function() {
+  this.data.channel = new Channel();
+});
+
 Template.newchannel.events({
 
   'change input': function(e, tmpl) {
     e.preventDefault();
 
-    if (!this.channel) 
-      this.channel = new Channel();
-
     var field = e.currentTarget;
     this.channel.set(field.id, field.value);
-    console.log(this.channel);
   },
 
   'submit form': function(e, tmpl) {
     e.preventDefault();
 
-    $("#add-channel").validate();
-    Meteor.call('/channels/new', this.channel, function(err, res) {
-      if (err) this.channel.catchValidationException();
-      FlowRouter.go('/channels/' + res._id);
-    });
+    if (this.channel.validate()) {
+      Meteor.call('/channels/new', this.channel, function(err, res) {
+        if (err) this.channel.catchValidationException();
+        FlowRouter.go('/channels/' + res._id);
+      });
+    }
   }
 
 });
