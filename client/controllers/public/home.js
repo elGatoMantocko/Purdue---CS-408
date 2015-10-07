45fe2e05811488129cb932fbec064826ae51e446
Elliott Mantock <emantock@gmail.com>
2015-10-07 07:34:16 -0700

    show separate lists for subscribed channels

--- client/controllers/public/home.js
+++ client/controllers/public/home.js
@@ -1,9 +1,37 @@
 Template.home.onCreated(function() {
-  Meteor.subscribe('latestChannels');
+  var self = this;
+  self.autorun(function() {
+    Meteor.subscribe('latestChannels');
+    Meteor.subscribe('myChannels');
+  });
 });
 
 Template.home.helpers({
+  mysubs: function() {
+    var user = User.findOne(Meteor.userId());
+    return user.getSubscriptions().fetch().sort(function(a, b) {
+      return (a.title).localeCompare(b.title);
+    });
+  },
   channels: function() {
-    return Channel.getLatest().fetch();
+    var user = User.findOne(Meteor.userId());
+    if (user) {
+      console.log(user.getSubscriptions().fetch());
+      return _.difference(Channel.getLatest().fetch(), user.getSubscriptions().fetch());
+    }
+    else {
+      return Channel.getLatest().fetch();
+    }
+  }
+});
+
+Template.subscribeButton.events({
+  'click button#subscribe': function(e, tmpl) {
+    e.preventDefault();
+
+    var user = User.findOne(Meteor.userId());
+    user.subscribeTo(this);
+    if (user.validate())
+      user.save();
   }
 });
