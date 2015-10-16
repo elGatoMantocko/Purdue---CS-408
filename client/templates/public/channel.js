26968d24b588c39dc30fe95afd56780f6156fe6d
Hunter Martin <sunnyboy451@gmail.com>
2015-10-15 23:16:31 -0400

    Added the preview helper and put a new div on the channels page

--- client/templates/public/channel.js
+++ client/templates/public/channel.js
@@ -10,5 +10,12 @@ Template.channel.helpers({
   channel: function() {
     var channelId = FlowRouter.getParam('id');
     return Channel.findOne(channelId);
+  },
+
+  preview: function() {
+    var channel = Channel.findOne(FlowRouter.getParam('id'));
+    var urls = Meteor.call('/channels/getUrls', channel);
+    urls = urls.splice(10, urls.length - 10);
+    return urls;
   }
 });
